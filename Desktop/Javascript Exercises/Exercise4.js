ec2f7238ea3b1b1b8a3c50f09920cf939b405479
HugoDellArciprete <hugojdellarci@gmail.com>
2021-03-29 16:08:01 -0300

    exercise 4 added

---
+++ Desktop/Javascript Exercises/Exercise4.js
@@ -0,0 +1,33 @@
+//** IF ELSE **//
+
+
+//* a)
+
+var randomNumber= Math.random ();
+console.log ("randomNumber");
+if (randomNumber >= 0,5) {
+  alert ("greater than 0,5");
+} else if (randomNumber <= 0,5) {
+  alert ("lower than 0,5");
+}
+
+
+//* b)
+
+var age= 88;
+if (age <2) {
+  alert ("Bebé");
+} else if (age <= 12) {
+  alert ("Nino");
+} else if (age <= 19) {
+  alert ("Adolescente");  
+} else if (age <= 30) {
+  alert ("Joven");
+} else if (age <= 60) {
+  alert ("Adulto");
+} else if (age <= 75) {
+  alert ("Adulto mayor");
+} else if (age >75) {
+  alert ("Anciano");
+}
+
