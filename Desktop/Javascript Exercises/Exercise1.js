db893eb0189811f4f4396b2024e2ad6cadaf5d55
HugoDellArciprete <hugojdellarci@gmail.com>
2021-03-29 16:05:35 -0300

    exercise 1 added

---
+++ Desktop/Javascript Exercises/Exercise1.js
@@ -0,0 +1,28 @@
+//** VARIABLES AND OPERATORS **//
+
+
+//* a)
+
+var number1 = 38;
+var number2 = 25;
+var number3 = number1 + number2
+console.log ("The result of the sum is: " + number3);
+
+
+//* b)
+
+var string1 = "qa ";
+var string2 = "automation";
+var string3 = string1 + string2;
+console.log ("The result of the concatenation is: " + string3);
+
+
+//* c)
+
+var word1 = "event";
+var word2 = "horizon";
+var fullWord = word1.length + word2.length;
+console.log("The number of letters is: " + fullWord);
+
+
+
